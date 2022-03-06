9f89ae82b974bb51500ddf3ce35e75b054e79f51
iamLolade <adeniyiotemade@gmail.com>
2022-03-06 23:18:34 +0100

    Added event listener to item prices and total cost

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 document.getElementById("btn").addEventListener("click", calculate)
 let costEl = document.getElementById("cost-el")
+let foodPrice = document.getElementById("food-select")
+let transportPrice = document.getElementById("transport-select")
+let total = document.getElementById("cost-el")
+let error = document.getElementById("error")
+
+function calculate() {
+
+}
